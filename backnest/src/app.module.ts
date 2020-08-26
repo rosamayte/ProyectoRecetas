6ab4afb59ef86dc64b4da139fcef5dd85100acad
Rosa <rosmayza798@gmail.com>
2020-08-26 16:32:08 -0400

    backnest recipes

--- backnest/src/app.module.ts
+++ backnest/src/app.module.ts
@@ -5,12 +5,15 @@ import { RecipesModule } from './recipes/recipes.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import {ServeStaticModule} from '@nestjs/serve-static'
+import { join } from 'path';
 
 @Module({
   imports: [
     RecipesModule, 
     UsersModule,
     AuthModule,
+    ServeStaticModule.forRoot({rootPath: join(__dirname,'..','public')}),
     MongooseModule.forRoot('mongodb://localhost:27017/recipes', 
     {useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
   ],
