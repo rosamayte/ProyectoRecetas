6878fba333c39cfc12accba6a571fc4fbb4bdd41
Rosa <rosmayza798@gmail.com>
2020-09-03 14:58:24 -0400

    main

--- frontend/src/app/pages/main/main.component.ts
+++ frontend/src/app/pages/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipesService } from 'src/app/services/recipes.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  private apidata: Array<any>;
+  public showdata: Array<Array<any>> = [];
+  public quantity: number;
+  public imagesUrl = environment.images_url;
+
+  public pagination = {
+    size: 20,
+    page: 0
+  }
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private recipeService: RecipesService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.recipeService.getRecipes().subscribe((data: any) => {
+      this.apidata = data.body;
+      this.quantity = data.body.length;
+      this.paginate(data.body);
+    }, error => {
+      console.log(error);
+    });
   }
+
+  private paginate = async (array: Array<any>): Promise<void> => {
+    const pages = await Math.ceil(array.length / this.pagination.size);
+    this.showdata = new Array<Array<any>>(pages);
+    this.quantity = array.length;
+    for (let i = 0; i < pages; i++) {
+      const start = await this.pagination.size * i
+      this.showdata[i] = await new Array<any>(...array.slice(start, start + this.pagination.size));
+    }
+  }
+
+  public goToRecipe(id: string) {
+    this.router.navigate(['recipe', id]);
+  }
+ 
 
 }
